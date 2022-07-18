092fe380bddd093e9cd409517e1667a8729bfe34
erikalves12 <97927458+erikalves12@users.noreply.github.com>
2022-07-18 19:32:23 -0300

    create documents

---
+++ src/personagem/personagem.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const PersonagemSchema = new mongoose.Schema({
+    NomePersonagem: {
+      type: String,
+      require: true,
+    },
+    ImagemPersonagem: {
+      type: String,
+      require: true,
+    },
+  });
+  
+  const Personagem = mongoose.model('Personagem', PersonagemSchema);
+  
+  module.exports = Personagem;
+  
---
+++ src/users/Users.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    select: false,
+  },
+  avatar: {
+    type: String,
+    require: true,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+
+module.exports =  User
+  
