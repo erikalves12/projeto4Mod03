0917984b10b973b1e809070ee88bf86685a4fda0
erikalves12 <97927458+erikalves12@users.noreply.github.com>
2022-07-19 08:43:05 -0300

    add auth route login

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,8 @@
+const authService = require("./auth.service")
+
+const loginController = async (req, res) =>{
+    res.send({message: "login Ok.."})
+
+    }
+
+    module.exports = {loginController}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDatabase = require("./database/database");
 const usersRoutes = require("./users/users.routes");
+const authRoutes = require("./auth/auth.route");
 
 const port = process.env.PORT || 3001;
 const app = express();
@@ -10,8 +11,9 @@ const app = express();
 connectDatabase();
 app.use(cors());
 app.use(express.json());
-app.use("/users", usersRoutes);
 
+app.use("/users", usersRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(port, (req, res) => {
   console.log(`Servidor rodando na porta ${port} `);
